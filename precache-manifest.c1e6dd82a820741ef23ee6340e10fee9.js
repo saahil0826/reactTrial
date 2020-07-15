b1bc9152e15b8cee858987010bcc94571dcc9dfe
saahil0826 <snawalkha0826@gmail.com>
2020-07-15 17:19:53 +0530

    Updates

---
+++ precache-manifest.c1e6dd82a820741ef23ee6340e10fee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5357df333cfebc58907a0aaa17de5e",
+    "url": "/reactTrial/index.html"
+  },
+  {
+    "revision": "ffdb2db3b873b2db69f6",
+    "url": "/reactTrial/static/css/main.73f8dc0c.chunk.css"
+  },
+  {
+    "revision": "572d27cb9fb9f4d9ed2a",
+    "url": "/reactTrial/static/js/2.0ca3f07a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTrial/static/js/2.0ca3f07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdb2db3b873b2db69f6",
+    "url": "/reactTrial/static/js/main.6609d24b.chunk.js"
+  },
+  {
+    "revision": "6f8886c7fc8a0d6b37e9",
+    "url": "/reactTrial/static/js/runtime-main.c0b78f0a.js"
+  }
+]);
